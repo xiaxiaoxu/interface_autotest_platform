fc028091f49c1b124840f5f2a8c20ac03e519b7b
xiaxiaoxu <xiaxiaoxu1987@163.com>
2021-05-18 20:59:20 +0800

    登录页面样式调整、导航栏切换样式调整

--- interface_auto_test_platform/settings.py
+++ interface_auto_test_platform/settings.py
@@ -124,14 +124,15 @@ USE_L10N = True
 USE_TZ = False
 
 
+LOGIN_URL = '/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-LOGIN_URL = '/login/'
 
 
 import pymysql         # 一定要添加这三行！
